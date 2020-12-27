20886812144149b089b8597f367036bf34ac34ad
PetrosGiatroutsikos <petrosgiatroutsikos@gmail.com>
2020-12-27 12:55:49 +0200

    push to save navigation (mostly)

---
+++ Project Volo/admin-login.php
@@ -0,0 +1,118 @@
+<?php include 'phpconnect.php';?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\bootstrap.min.css.map">
+    <link rel="stylesheet" href="css\webfonts\all.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+	<!-- Navbar-->
+    <nav style="background-color:powderblue;" class="navbar navbar-expand-sm bg-menu-light navbar-light box-shadow navbar-static-top" id="nav">
+        <ul class="navbar-nav nav-list">
+            <li class="nav-item">
+            <a class="toggle-btn nav-link" href="#">
+                <i class="fa fa-bars d-xs-inline d-md-none" id="sidebtn"> </i>
+            </a>
+            </li>
+            <li class="nav-item">
+				<a class="nav-link d-none d-md-inline-block" href="home.html"><i class="fa fa-home"></i><span class="d-none d-lg-inline"> Αρχική </span></a>
+            </li>
+            <li class="nav-item">
+				<a class="nav-link d-none d-md-inline-block" href="navigation.php"><i class="fas fa-hand-holding-usd"></i><span class="d-none d-lg-inline"> Δωρεές </span></a>
+            </li>
+
+        </ul>
+        <!-- Logo -->
+        <a class="navbar-brand logo justify-content-center" href="#">
+            <img src="images\logo.png"  alt="YesPolitics">
+        </a>
+        <div class="ml-auto justify-content-end">
+            <a class="navbar-brand" href="#">#DoreesVolo</a>
+        </div>
+    </nav>
+    <body style="background-color:#A6D4DA;">
+		<div class="container">
+    <div class="row article-view">
+      <div class="card article col">
+        <div class="card-body">
+          <div class="article-title"><h1>FAGHTO GIA PAIDIA</h1></div>
+          <div class="article-subtitle"><h2>VOITHAME PAIDAKIA DINONTAS TA FAGHTO</h2></div>
+            <div class="article-image">
+                <img src="https://projectdonations.000webhostapp.com/images/mainimg1.jpg" class="img-fluid">
+                <h5>
+                  <a href="google.com">SYRIA</a>
+                </h5>
+              </div>
+                  <p class="card-text author-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae consequat enim, ut tempor quam. Vestibulum at lectus ac erat pulvinar feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc volutpat, eros condimentum posuere fringilla, tortor tortor lobortis felis, non hendrerit quam massa ultrices turpis. Aenean aliquet congue ligula. Cras sed molestie sapien. Nunc dapibus consequat leo, at sodales ligula vestibulum quis. Integer suscipit neque metus, et faucibus elit ullamcorper at. Donec eget lacinia ipsum, ac malesuada eros. Fusce sagittis ante nec aliquam congue. Mauris quis tortor eget ipsum congue egestas. Pellentesque sit amet congue augue. Nunc dignissim, lacus vitae feugiat iaculis, justo enim blandit sem, ac suscipit leo ante in tellus. Aenean sollicitudin purus in risus tempor ultrices. Cras a enim finibus, suscipit massa pharetra, sollicitudin turpis. Maecenas vitae pulvinar urna, quis placerat nisl.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis dignissim nulla libero, sed sodales tortor convallis non. Quisque varius mattis facilisis. Aliquam finibus libero eros, in pulvinar ante imperdiet tempus. Curabitur quis pulvinar lacus. Pellentesque et accumsan leo. Morbi faucibus dapibus nunc, nec malesuada leo rutrum eu. Mauris consequat posuere arcu. Praesent ultricies laoreet neque, eu vehicula metus. Sed vitae maximus tortor. Nulla velit felis, aliquet sed sem lobortis, tristique consectetur lorem. Praesent a laoreet tellus. Donec id risus eget arcu iaculis lacinia nec et nisi. Nam arcu erat, placerat sit amet convallis et, hendrerit ac orci.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur lorem quam, varius eu vehicula et, bibendum vel ante. Vivamus pretium velit at sodales maximus. Donec nisl enim, dictum a nisi eget, pulvinar vehicula risus. Sed dignissim porttitor libero, at maximus turpis fermentum ut. Sed eleifend tincidunt posuere. Vestibulum tortor ipsum, vestibulum nec dolor vitae, condimentum finibus nulla. Nulla facilisi.
+                        Morbi vestibulum condimentum libero semper viverra. Pellentesque porttitor velit fermentum pretium maximus. Cras cursus congue nisi, ac facilisis elit pulvinar eu. Phasellus neque mi, aliquam quis dolor vitae, ultricies sodales augue. Etiam vel felis id arcu blandit ornare. Donec semper metus ac lorem rutrum viverra. Duis efficitur ante ac dictum dictum. Ut malesuada fringilla placerat. Vestibulum at elit tellus. Vivamus malesuada pulvinar magna, quis tempor est vehicula eu. Cras lobortis pretium tortor eget vehicula. Mauris volutpat tincidunt mi nec blandit. Nulla rhoncus a leo a egestas. Nullam at libero gravida turpis mollis posuere.
+                        Morbi sed pretium urna, vel tempus nibh. Quisque non lorem sed neque consectetur euismod. Vestibulum euismod ut dolor sit amet dignissim. Donec semper mi a magna pharetra, in ultrices urna cursus. Aenean fringilla metus metus, ut porta mi aliquam sit amet. Pellentesque viverra, lectus sed laoreet facilisis, ligula mi sollicitudin turpis, eu ultrices augue tellus a odio. Curabitur rutrum maximus imperdiet. Nam ultrices metus quis eros bibendum consequat. Ut iaculis maximus euismod.</p>
+                         <a href="https://projectdonations.000webhostapp.com/addDonation.php" class="btn btn-green" style="float: right;">Ξεκινήστε την δωρεά! <i class="fas fa-donate"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+    </body>
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="css\main.css">
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\bootstrap.min.css.map">
+    <link rel="stylesheet" href="css\webfonts\all.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+	<!-- Navbar-->
+    <nav style="background-color:powderblue;" class="navbar navbar-expand-sm bg-menu-light navbar-light box-shadow navbar-static-top" id="nav">
+        <ul class="navbar-nav nav-list">
+            <li class="nav-item">
+            <a class="toggle-btn nav-link" href="#">
+                <i class="fa fa-bars d-xs-inline d-md-none" id="sidebtn"> </i>
+            </a>
+            </li>
+            <li class="nav-item">
+				<a class="nav-link d-none d-md-inline-block" href="home.html"><i class="fa fa-home"></i><span class="d-none d-lg-inline"> Αρχική </span></a>
+            </li>
+            <li class="nav-item">
+				<a class="nav-link d-none d-md-inline-block" href="navigation.php"><i class="fas fa-hand-holding-usd"></i><span class="d-none d-lg-inline"> Δωρεές </span></a>
+            </li>
+
+        </ul>
+        <!-- Logo -->
+        <a class="navbar-brand logo justify-content-center" href="#">
+            <img src="images\logo.png"  alt="YesPolitics">
+        </a>
+        <div class="ml-auto justify-content-end">
+            <a class="navbar-brand" href="#">#DoreesVolo</a>
+        </div>
+    </nav>
+    <body style="background-color:#A6D4DA;">
+		<div class="container">
+    <div class="row article-view">
+      <div class="card article col">
+        <div class="card-body">
+          <div class="article-title"><h1>FAGHTO GIA PAIDIA</h1></div>
+          <div class="article-subtitle"><h2>VOITHAME PAIDAKIA DINONTAS TA FAGHTO</h2></div>
+            <div class="article-image">
+                <img src="https://projectdonations.000webhostapp.com/images/mainimg1.jpg" class="img-fluid">
+                <h5>
+                  <a href="google.com">SYRIA</a>
+                </h5>
+              </div>
+                  <p class="card-text author-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae consequat enim, ut tempor quam. Vestibulum at lectus ac erat pulvinar feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc volutpat, eros condimentum posuere fringilla, tortor tortor lobortis felis, non hendrerit quam massa ultrices turpis. Aenean aliquet congue ligula. Cras sed molestie sapien. Nunc dapibus consequat leo, at sodales ligula vestibulum quis. Integer suscipit neque metus, et faucibus elit ullamcorper at. Donec eget lacinia ipsum, ac malesuada eros. Fusce sagittis ante nec aliquam congue. Mauris quis tortor eget ipsum congue egestas. Pellentesque sit amet congue augue. Nunc dignissim, lacus vitae feugiat iaculis, justo enim blandit sem, ac suscipit leo ante in tellus. Aenean sollicitudin purus in risus tempor ultrices. Cras a enim finibus, suscipit massa pharetra, sollicitudin turpis. Maecenas vitae pulvinar urna, quis placerat nisl.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis dignissim nulla libero, sed sodales tortor convallis non. Quisque varius mattis facilisis. Aliquam finibus libero eros, in pulvinar ante imperdiet tempus. Curabitur quis pulvinar lacus. Pellentesque et accumsan leo. Morbi faucibus dapibus nunc, nec malesuada leo rutrum eu. Mauris consequat posuere arcu. Praesent ultricies laoreet neque, eu vehicula metus. Sed vitae maximus tortor. Nulla velit felis, aliquet sed sem lobortis, tristique consectetur lorem. Praesent a laoreet tellus. Donec id risus eget arcu iaculis lacinia nec et nisi. Nam arcu erat, placerat sit amet convallis et, hendrerit ac orci.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur lorem quam, varius eu vehicula et, bibendum vel ante. Vivamus pretium velit at sodales maximus. Donec nisl enim, dictum a nisi eget, pulvinar vehicula risus. Sed dignissim porttitor libero, at maximus turpis fermentum ut. Sed eleifend tincidunt posuere. Vestibulum tortor ipsum, vestibulum nec dolor vitae, condimentum finibus nulla. Nulla facilisi.
+                        Morbi vestibulum condimentum libero semper viverra. Pellentesque porttitor velit fermentum pretium maximus. Cras cursus congue nisi, ac facilisis elit pulvinar eu. Phasellus neque mi, aliquam quis dolor vitae, ultricies sodales augue. Etiam vel felis id arcu blandit ornare. Donec semper metus ac lorem rutrum viverra. Duis efficitur ante ac dictum dictum. Ut malesuada fringilla placerat. Vestibulum at elit tellus. Vivamus malesuada pulvinar magna, quis tempor est vehicula eu. Cras lobortis pretium tortor eget vehicula. Mauris volutpat tincidunt mi nec blandit. Nulla rhoncus a leo a egestas. Nullam at libero gravida turpis mollis posuere.
+                        Morbi sed pretium urna, vel tempus nibh. Quisque non lorem sed neque consectetur euismod. Vestibulum euismod ut dolor sit amet dignissim. Donec semper mi a magna pharetra, in ultrices urna cursus. Aenean fringilla metus metus, ut porta mi aliquam sit amet. Pellentesque viverra, lectus sed laoreet facilisis, ligula mi sollicitudin turpis, eu ultrices augue tellus a odio. Curabitur rutrum maximus imperdiet. Nam ultrices metus quis eros bibendum consequat. Ut iaculis maximus euismod.</p>
+                         <a href="https://projectdonations.000webhostapp.com/addDonation.php" class="btn btn-green" style="float: right;">Ξεκινήστε την δωρεά! <i class="fas fa-donate"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+    </body>
+</html>
--- Project Volo/navigation.php
+++ Project Volo/navigation.php
@@ -1,10 +1,10 @@
 <?php
-	include 'https://projectdonations.000webhostapp.com/phpconnect.php';
+	include 'phpconnect.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-    <link rel="stylesheet" href="css\main.css">
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="css\bootstrap.min.css">
     <link rel="stylesheet" href="css\bootstrap.min.css.map">
     <link rel="stylesheet" href="css\webfonts\all.min.css">
@@ -19,7 +19,7 @@
             </a>
             </li>
             <li class="nav-item">
-				<a class="nav-link d-none d-md-inline-block" href="home.html"><i class="fa fa-home"></i><span class="d-none d-lg-inline"> Αρχική </span></a>
+				<a class="nav-link d-none d-md-inline-block" href="index.html"><i class="fa fa-home"></i><span class="d-none d-lg-inline"> Αρχική </span></a>
             </li>
             <li class="nav-item active">
 				<a class="nav-link d-none d-md-inline-block"><i class="fas fa-hand-holding-usd"></i><span class="d-none d-lg-inline"> Δωρεές </span></a>
@@ -35,24 +35,65 @@
         </div>
     </nav>
 	
-    <body style="background-color:A6D4DA;">
+    <body style="background-color:#A6D4DA;">
 		<h1 align="center" >Λίστα Δωρεών:</h1> 
         <div class="container-fluid">
             <div class="row">
                 <div class="card-columns"> 
-				<!-- edw tha mpei kwdikas php pou tha metraei poses anages tha yparxoun sthn vash gia na kanoume kai toses karteles-->
-                        <div class="card article-card">
-                            <div class="card-body">
-                                <h5 class="card-title">SAMPLE TITLE</h5>
-                                <p class="card-text">FIRST ROW FIRST ROW FIRST ROW FIRST ROW FIRST ROW </p>
-                                <a href="https://www.google.gr/" class="btn btn-green">Περισσότερα</a>
+				<?php
+				    $sql="SELECT COUNT(NeedID) AS NeedNO FROM Needs;";
+                    $result = mysqli_query($con,$sql);
+                    $row=mysqli_fetch_array($result);
+                    $needs_number=$row[0];
+                    /*for ($x = 0; $x < $needs_number; $x++) {
+                        echo $card;
+                    }*/
+                ?>
+                
+                <?php  
+                        $rowNeedName=array_fill (0,$needs_number,'0');
+                        $rowGoal=array_fill (0,$needs_number,'0');
+                        $rowLocation=array_fill (0,$needs_number,'0');
+                        $rowHTMLlink=array_fill (0,$needs_number,'0');
+                        $rowImage=array_fill (0,$needs_number,'0');
+                        $rowDateCreated=array_fill (0,$needs_number,'0');
+                        $rowFundsCollected=array_fill (0,$needs_number,'0');
+                        
+                        $data="SELECT * from Needs;";
+                        $result2 = mysqli_query($con,$data);
+                        $counter=0;
+                        while($row2= mysqli_fetch_array($result2)){
+                             $rowNeedName[$counter]=$row2['1'];
+                             $rowLocation[$counter]=$row2['2'];
+                             $rowGoal[$counter]=$row2['3'];
+                             $rowHTMLlink[$counter]=$row2['4'];
+                             $rowImage[$counter]=$row2['5'];
+                             $rowDateCreated[$counter]=$row2['6'];
+                             $rowFundsCollected[$counter]=$row2['7'];
+                             $counter++;
+                        }
+                        
+                        for ($i = 0; $i < $needs_number; $i++)
+                        {
+                            $percentageGoal=$rowFundsCollected[$i]*100/$rowGoal[$i];
+                            $card = '<div class="card article-card text-right">
+                              <img class="card-img-top" src="'.$rowImage[$i].'">
+                              <div class="progress">
+                              <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: '.$percentageGoal.'%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">'.$percentageGoal.'%</div>
+                            </div>
+                            <div class="card-body text-left">
+                            
+                               <h5 class="card-title">'.$rowNeedName[$i].'</h5>
+                                <p class="card-text">'.$rowLocation[$i].'</p>
+                                <a href="'.$rowHTMLlink[$i].'" class="btn btn-green">Δωρίστε Τώρα! <i class="fas fa-donate"></i></a>
                             </div>
                             <div class="card-footer text-muted">
-                                <small>από <a href="https://www.google.gr/">PETROS GIATROUTSIKOS</a>, </small>
-                                <small class="text-right">10/10/2010</small>
+                                <small class="text-right">Ημερομηνία δημιουργίας: '.$rowDateCreated[$i].'</small>
                             </div>
-                        </div> 
-						
+                        </div>';
+                        echo $card;
+                        }
+                ?>                        
 				</div>
             </div>
 		</div>
